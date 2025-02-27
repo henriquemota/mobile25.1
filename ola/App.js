5fdb155523288a3844b688685498d148f42ebe10
henriquemota <mota.henrique@gmail.com>
2025-02-27 08:39:47 -0300

    atualizando

--- ola/App.js
+++ ola/App.js
@@ -1,21 +1,30 @@
 import React from 'react'
 import { Text, View } from 'react-native'
 
-export default function App() {
-	const data = []
-	for (let i = 0; i < 100; i++) data.push(`elemento de número ${i + 1}`)
+const container = {
+	flex: 1,
+	justifyContent: 'center',
+}
+const text = {
+	padding: 4,
+	margin: 10,
+	fontSize: 32,
+	backgroundColor: 'yellow',
+}
 
+const data = []
+for (let i = 0; i < 10; i++) data.push(`item de número ${i + 1}`)
+
+export default function App() {
 	return (
-		<View style={{ flex: 1, gap: 4 }}>
-			<View>
-				{data.map(function (element, index) {
-					return (
-						<View key={index} style={{ backgroundColor: 'yellow', padding: 4 }}>
-							<Text>{element}</Text>
-						</View>
-					)
-				})}
-			</View>
+		<View key={1} style={container}>
+			{data.map(function (element, index) {
+				return (
+					<Text key={index} style={text}>
+						{element}
+					</Text>
+				)
+			})}
 		</View>
 	)
 }
